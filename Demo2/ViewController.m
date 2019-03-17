b5107b4302a7898e8e40a1d90203eb40b5cc2b74
TT-struggleofoneself <504470358@qq.com>
2019-03-17 13:32:00 +0800

    OC与JS多参数交互

--- Demo2/ViewController.m
+++ Demo2/ViewController.m
@@ -7,16 +7,46 @@
 //
 
 #import "ViewController.h"
+#import "TestJSObject.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UIWebViewDelegate>
+@property(nonatomic,strong)UIWebView* webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.webView=[[UIWebView alloc]initWithFrame:self.view.bounds];
+    self.webView.delegate=self;
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+    [self.view addSubview:self.webView];
+    
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    //网页加载完成调用此方法
+    
+    //首先创建JSContext 对象（此处通过当前webView的键获取到jscontext）
+    JSContext *context=[webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    
+    //第二种情况，js是通过对象调用的，我们假设js里面有一个对象 testobject 在调用方法
+    //首先创建我们新建类的对象，将他赋值给js的对象
+    
+    TestJSObject *testJO=[TestJSObject new];
+    context[@"testobject"]=testJO;
+    
+    //同样我们也用刚才的方式模拟一下js调用方法
+    NSString *jsStr1=@"testobject.TestNOParameter()";
+    [context evaluateScript:jsStr1];
+    NSString *jsStr2=@"testobject.TestOneParameter('参数1')";
+    [context evaluateScript:jsStr2];
+    NSString *jsStr3=@"testobject.TestTowParameterSecondParameter('参数A','参数B')";
+    [context evaluateScript:jsStr3];
+    
 }
 
 
